a38a18fff2a5b81be80e9711ee5704cf7d3250fd
MEKL20 <edwinkhoirudin16@gmail.com>
2022-03-07 14:24:53 +0700

    wakandas v3

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-function calculateTax(annualIncome, status, childrenCount=0) {
+function calculateTax(taxYear, name, annualIncome, marriageStatus, dependentChildrenCount) {
     let ptkp = 50000000  
-    if (status == "Lajang") ptkp
-    else if (status == "Menikah") ptkp = 60000000 + (15000000 * childrenCount)
-    else if (status == "Cerai") ptkp = 70000000 + (15000000 * childrenCount)
+    if (marriageStatus == "Lajang") ptkp
+    else if (marriageStatus == "Menikah") ptkp = 60000000 + (15000000 * dependentChildrenCount)
+    else if (marriageStatus == "Cerai") ptkp = 70000000 + (15000000 * dependentChildrenCount)
 
     let pkp = annualIncome - ptkp
     let pph = pkp * .1
 
-    if ((pkp - 200000000) > 0) pph += (pkp - 200000000) * .2
-    if ((pkp - 250000000) > 0) pph += (pkp - 250000000) * .3
+    if ((pkp - 200000000) > 0) {
+        pkp -= 200000000
+        pph += pkp * .2
+    }
+
+    if ((pkp - 250000000) > 0) {
+        pkp -= 250000000
+        pph += pkp * .3
+    }
 
     return pph
 }
