2e5ca00b6743b223f6028f0878cd11c5a441e865
MEKL20 <edwinkhoirudin16@gmail.com>
2022-03-07 11:43:23 +0700

    wakandas v1

---
+++ main.js
@@ -0,0 +1,11 @@
+function calculateTax(annualIncome) {
+    let pph = annualIncome - 50000000
+
+    if (pph < 0)return 0
+    else if (pph < 200000001) return pph *= .1
+    else if (pph < 450000001) return pph *= .15
+    else if (pph > 450000000) return pph *= .2
+
+}
+
+console.log(calculateTax(1000000000));
